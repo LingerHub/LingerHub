63e03a92f8da9433ceed406573e66dab1c6f0770
Stéphane Bisinger <stephane.bisinger@protonmail.com>
2018-08-28 21:45:19 +0200

    Make test fail

--- test/lingerhub_web/controllers/page_controller_test.exs
+++ test/lingerhub_web/controllers/page_controller_test.exs
@@ -2,7 +2,7 @@ defmodule LingerhubWeb.PageControllerTest do
   use LingerhubWeb.ConnCase
 
   test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    conn = get(conn, "/")
+    assert html_response(conn, 201) =~ "Welcome to Phoenix!"
   end
 end
